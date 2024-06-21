05fa10f17e5954bddfbb38f4c3d4601e805c9055
Julian Mercado <julianmercadomeza@gmail.com>
2024-06-21 19:50:44 -0400

    Updated portfolio and contact form was removed

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+  footerCopyright = ' Julian Mercado Portfolio | Copyright 2024';
+}
